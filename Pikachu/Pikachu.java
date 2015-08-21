888fd092b517f565b2793f9c100b4f8b822a5b97
Hannahcp822 <hannah123p@gmail.com>
2015-08-21 08:46:24 -0700

    08/21/15 Pikachu!
    :)

---
+++ Pikachu/Pikachu.java
@@ -0,0 +1,18 @@
+
+/**
+ * Display Pikachu! 
+ * 
+ * @author Hannah Pang
+ * @version August 21, 2015
+ */
+public class Pikachu
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Pikachu welcomes you to the world of Pokemon!");
+        System.out.println("\t (\\__/)");
+        System.out.println("\t (o^.^)");
+        System.out.println("\tz(_(\")(\")");
+    }
+}
+      
