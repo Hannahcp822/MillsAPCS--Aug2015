0de93ad113060616b807b1230ba5f199ad315ab4
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2015-08-25 23:08:03 -0700

    08/25/15 - FlySong
    P1.3 - “There Was an Old Lady Who Swallowed a Fly”

---
+++ Fly Song/FlySong.java
@@ -0,0 +1,12 @@
+
+/**
+ * P1.3 - "There Was an Old Lady Who Swallowed a Fly"
+ * 
+ * @author Hannah Pang
+ * @version August 26, 2015
+ */
+public class FlySong
+{
+    public static void main(String[] args)
+    {
+        
