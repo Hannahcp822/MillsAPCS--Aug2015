8d70e40ac2b7393d28bc18c5264ecf70f4c94027
Hannahcp822 <hannah123p@gmail.com>
2015-08-27 08:55:51 -0700

    08/27/15 Letters P1.2
    Bonny Lee and Hannah Pang

---
+++ Letters/Letters.java
@@ -0,0 +1,90 @@
+
+/**
+ * Letters - P1.2
+ * 
+ * @author Bonny Lee and Hannah Pang 
+ * @version August 27, 2015
+ */
+public class Letters
+{
+    public static void main(String[] args)
+    {
+        Brother();
+        Sister();
+        Mom();
+    }
+    
+    public static void Brother()
+    {
+        GreetingBrother();
+        Classes();
+        Sports();
+        Movies();
+        Ending();
+    }
+    
+    public static void Sister()
+    {
+        GreetingSister();
+        Classes();
+        Friends();
+        Clothes();
+        Ending();
+    }
+    
+    public static void Mom()
+    {
+        GreetingMom();
+        Classes();
+        Friends();
+        Movies();
+        Ending();
+    }
+    
+    public static void GreetingBrother()
+    {
+        System.out.println("Hey bro! How have you been? I have some questions for you.");
+    }
+    
+    public static void GreetingSister()
+   
+        System.out.println("Hey sis! How's college? I was wondering if I could get your advice on some things.");
+    }
+    
+    public static void GreetingMom()
+    {
+        System.out.println("Hey mom! How's everything going? I need to ask you some things.");
+    }
+    
+    public static void Classes()
+    {
+        System.out.println("So it's time for me to choose classes for the school year... I'm really into writing narratives, but I'm not sure if I should take AP English beecause I heard it's a lot of work. What do you think?");
+    }
+    
+    public static void Sports()
+    {
+        System.out.println("I also have to prepare for volleyball tryouts, which start next Monday. I haven't played that much over the summer and am a little worried about making the team. Got any advice?");
+    }
+    
+    public static void Movies()
+    {
+        System.out.println("So it's been really hot lately and I was thinking about going to see a movie one of these days. Have you seen any good ones recently?");
+    }
+    
+    public static void Friends()
+    {
+        System.out.println("I'm excited about seeing everyone back at school over the summer, but a few of my friends and I have been fighting recently. I don't think it was my fault but what should I do?");
+    }
+    
+    public static void Clothes()
+    {
+        System.out.println("Thanks for letting me borrow your shirt this summer! I promise to return it soon!");
+    }
+    
+    public static void Ending()
+    {
+        System.out.println("Write me back and see you soon!!");
+    }
+}
+
+        
